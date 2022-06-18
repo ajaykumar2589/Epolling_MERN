4af506621c25a22934288a47ecc49beb55ebbb97
Ajay <chidiralaajay9@gmail.com>
2022-06-18 16:31:44 +0530

    secondcommit

--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
 const express=require('express');
 const app=express();
 const port=3000;
+const handle=require('./handlers');
 app.get('/',(req,res)=>res.json({hello:'world'}));
-app.use((req,res,next)=>{
-    const err= new Error('Not Found');
-    err.status=404;
-    next(err);
-});
-app.use((err,req,res,next)=>{
-     res.status(err.status || 500).json({
-    err:err.message || 'Something went wrong'
-     });
-});
+app.use(handle.notFound);
+app.use(handle.errors);
 
 app.listen(port,console.log(`Server started on port ${port} `));
