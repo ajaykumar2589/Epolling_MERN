f33c0154da99968792e2ca7cb1c969e12cd3790b
Ajay <chidiralaajay9@gmail.com>
2022-06-18 16:45:35 +0530

    thridcommit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const express=require('express');
-const app=express();
-const port=3000;
+const cors=require('cors');
+const bodyParser=require('body-parser');
 const handle=require('./handlers');
+const app=express();
+const port=process.env.PORT ;
+app.use(cors());
+app.use(bodyParser.json());
 app.get('/',(req,res)=>res.json({hello:'world'}));
 app.use(handle.notFound);
 app.use(handle.errors);
